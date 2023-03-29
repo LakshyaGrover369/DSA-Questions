467e0c6c13fb2c65002dd1d5ad8adf8c236d31a5
Alien <groverlakshya.25.lg@gmail.com>
2023-03-29 22:25:49 +0530

    second commit

--- intersection_of_arrays.cpp
+++ intersection_of_arrays.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <algorithm>
+// #include <math.h>
 using namespace std;
 
 void arrayInput( int *arr , int n){  
@@ -7,51 +8,61 @@ void arrayInput( int *arr , int n){
          cin>>arr[i];
     }
 }
-void arrayInsertion( int *arr , int n , int m , int start){  
-    for(int i = start; i < n ; i++){
-         arr[i]+=m;
-    }
+int reverseNum(int n){
+	int temp,revNum=0;
+	while(n>0){
+		temp=n%10;
+		n/=10;
+		revNum=(revNum*10)+temp;
+	}
+	return revNum;
 }
 void arrayOutput( int *arr, int n){  
     for(int i = 0; i < n ; i++){
-         cout<<arr[i]<<" ";
+         cout<<arr[i]<<", ";
     }
 }
-void intersectionArray( int *arr1,int *arr2, int n){  
+void insertionArray( int *arr1,int *arr2, int n){  
         sort(arr1,arr1+n);
         sort(arr2,arr2+n);
-        int count=0,arr[n];  
-    for (int i = 0; i < n ; i++)
-    {    
-        for (int j = count; j < n; i++)
+        int count=0,arr[n],num=0,temp,temp2=0;  
+		cout<<"[";
+	for (int i = 0; i < n ; i++)
+    {   
+        for (int j = count; j < n; j++)
         {
            if(arr1[i]==arr2[j]) {
             count++;
-            // cout<<arr2[j]<<",";
-            arrayInsertion(arr,n,arr2[j],count);
-    
+			num=arr1[i]+(num*10);
+            break;
            }
         }
             
     }
-    arrayOutput(arr,n);
+			// cout<<num<<endl;
+	num=reverseNum(num);
+			// cout<<num<<endl;
+	while(num>0){
+			temp2++;
+		  temp=num%10;
+		  num/=10;
+		  cout<<temp;
+		  if(temp2!=count) cout<<", ";
+	}
+    // arrayOutput(arr,n);
+	cout<<"]";
 }
 
  int main() {
-    int n;
+    int n,m;
     cin>>n;
-
 	if(n>=1){
 		int arr1[n];
 		int arr2[n];
 		arrayInput(arr1,n);
 		arrayInput(arr2,n);
-        intersectionArray(arr1,arr2,n);
+        insertionArray(arr1,arr2,n);
+		// arrayOutput(arr,n);
     }
     return 0;
 }
-
-
-
-
-
