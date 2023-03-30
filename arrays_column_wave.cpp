daf2b41ffa088122bc8e3ed244f06eef752684d9
Alien <groverlakshya.25.lg@gmail.com>
2023-03-30 17:09:39 +0530

    column Wave Print

--- arrays_column_wave.cpp
+++ arrays_column_wave.cpp
@@ -2,34 +2,42 @@
 #include <algorithm>
 using namespace std;
 
-void arrayInput( int **arr , int m, int n){  
-    for(int i = 0; i < m ; i++){
-        for (int j = 0; j < n; j++)
-        {
-         cin>>arr[m][n];
-            
-        }
-        
-    }
-}
-
-// void arrayOutput( int *arr, int n){  
-//     for(int i = 0; i < n ; i++){
-//          cout<<arr[i]<<", ";
-//     }
-// }
-void columnWavePrint( int **arr,int m, int n){  
-        
-}
-
  int main() {
     int n,m;
     cin>>m;
     cin>>n;
-	if(m<10 && n<10){
+	if(m>1 && n>1 && m<10 && n<10){
 		int arr[m][n];
-		arrayInput(arr,m,n);
-        columnWavePrint(arr,m,n);
+
+		for(int i = 0; i < m ; i++)
+		{
+			for (int j = 0; j < n; j++)
+			{
+			cin>>arr[i][j];
+			}
+		}
+        for (int i = 0; i < n; i++)
+        {
+			// cout<<"i="<<i<<endl;
+            if(i%2==0)
+			{
+                for (int j = 0; j < m; j++)
+                {
+                    cout<<arr[j][i]<<", ";
+					// cout<<"j="<<j<<endl;
+                }
+            }
+            else
+			{
+                for (int j = m-1; j >= 0; j--)
+                {
+                    cout<<arr[j][i]<<", ";
+					// cout<<"j="<<j<<endl;
+                }
+            }
+        }
+		cout<<"END";
+
     }
     return 0;
 }
