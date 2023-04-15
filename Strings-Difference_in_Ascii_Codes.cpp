482ebe17f14b7354f19dff070ee6f3285e8e4475
Alien <groverlakshya.25.lg@gmail.com>
2023-04-15 10:44:04 +0530

    strings difference ascii codes

---
+++ Strings-Difference_in_Ascii_Codes.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+ int main() {
+    string s;
+    cin>>s;
+   
+    for (int i = 0; i < s.size(); i++)
+    {   
+        if(i==s.size()-1)   cout<<s[i];
+        else                cout<<s[i]<<s[i+1]-s[i];
+    }
+    
+    return 0;
+}
